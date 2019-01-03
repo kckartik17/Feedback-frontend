3a971c84e23a247f77318fe3aa3f9f7d80b0c74b
Kartik Chauhan <kckartik17@gmail.com>
2019-01-04 00:43:30 +0530

    header designed
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-`npm i --save redux react-redux react-router-dom`
+`npm i --save redux react-redux react-router-dom materialize-css`
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveryNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -11,8 +11,12 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
         </div>
       </BrowserRouter>
     </div>
